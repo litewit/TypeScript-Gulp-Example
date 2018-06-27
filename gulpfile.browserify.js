2f53a427fb33cfb53341f66c46a7235d662a4a7f
Mohammad Quadri <quadrixm@gmail.com>
2018-06-27 20:07:34 +0530

    Type script gulp browserify example

---
+++ gulpfile.browserify.js
@@ -0,0 +1,31 @@
+// This gulp script convert .ts to .js comman format for using in browser
+// To test rename is to gulpfile.js and run gulp
+
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream")
+var tsify = require("tsify")
+
+var paths = {
+    pages: ['src/*.html']
+}
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+})
+
+gulp.task("default", ["copy-html"], function () {
+
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+});
---
+++ gulpfile.node.js
@@ -0,0 +1,12 @@
+// This gulp script convert .ts to .js es5 format
+// To test rename is to gulpfile.js and run gulp
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
+});
